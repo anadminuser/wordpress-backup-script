d727f0397331ea9dd41c1110b83c792966a7e7c7
Travis <17461538+anadminuser@users.noreply.github.com>
2022-01-23 22:46:01 -0700

    Initial commit
    V1, slapped this together to get ready to migrate from a server. I see potential, I'll make updates as needed.

---
+++ backup-wordpress.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+start=`date +%s`
+time=$(date +"%T")
+echo "Started backup process at $time"
+
+echo "Sites found:"
+find ./public_html/ -type f -name "wp-config.php"
+
+
+if [ -d ~/script_backup_dir ] ; then 
+    echo "backing up in ~/script_backup_dir"
+else 
+    mkdir ~/script_backup_dir
+    echo "Creating ~/script_backup_dir/"
+fi 
+i=0
+
+for n in $( find ./public_html/ -type f -name "wp-config.php" )
+do
+    db_name=`grep 'DB_NAME' $n | sed "s/define(\s*'DB_NAME', '//g" | sed "s/'\s*);//g" | tr '\r' '\n'`
+    db_user=`grep 'DB_USER' $n | sed "s/define(\s*'DB_USER', '//g" | sed "s/'\s*);//g" | tr '\r' '\n'`
+    db_pass=`grep 'DB_PASSWORD' $n | sed "s/define(\s*'DB_PASSWORD', '//g" | sed "s/'\s*);//g" | tr '\r' '\n'`
+    db_host=`grep 'DB_HOST' $n | sed "s/define(\s*'DB_HOST', '//g" | sed "s/'\s*);//g" | tr '\r' '\n'`
+    name=`echo $n | sed 's/\.\/public_html\///g' | sed 's/\/wp-config\.php//g' | tr '\r' '\n'`
+    home=`dirname $n | tr '\r' '\n'`
+
+    mysqldump --no-tablespaces -u$db_user -p$db_pass -h$db_host $db_name > $home/wp-content/sql_dump.sql
+
+    if [ -f $home/wp-content/sql_dump.sql ] ; then 
+        echo "Database backup was created for $name"
+    else 
+        echo "Datbase backup was not created. Something went wrong."
+    fi 
+
+    cd ~/script_backup_dir
+    zip -qr "$name.zip" ~/$home/. 
+    if [ -f $name.zip ] ; then 
+        echo "backup file created"
+    else 
+        echo "Backup was not created. Something went wrong."
+    fi 
+    cd ~
+    ((i=i+1))
+    echo "Backups created: $i"
+done
+end=`date +%s`
+runtime=$((end-start))
+echo "$i backups were created in $runtime seconds."
